312125e311442434ccb667d2fa9fc3cae89e2d15
karolineconceicaodasilva <karolineconceicaodasilvanh@gmail.com>
2024-03-23 14:39:29 -0300

    created new project

---
+++ coin-mestre/src/main/java/com/projeto/coinmestre/CoinMestreApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.coinmestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinMestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinMestreApplication.class, args);
+	}
+
+}
---
+++ coin-mestre/src/test/java/com/projeto/coinmestre/CoinMestreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.coinmestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinMestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
