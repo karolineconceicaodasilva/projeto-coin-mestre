3fa9ab1f48648ddbb4b3424eef780b5f18fade65
karolineconceicaodasilva <karolineconceicaodasilvanh@gmail.com>
2024-03-23 15:43:31 -0300

    created model expense

---
+++ coin-mestre/src/main/java/com/projeto/coinmestre/domain/ExpenseStatus.java
@@ -0,0 +1,7 @@
+package com.projeto.coinmestre.domain;
+
+public enum ExpenseStatus {
+    OPEN,
+    CLOSE,
+    OVERDUE
+}
---
+++ coin-mestre/src/main/java/com/projeto/coinmestre/model/Expense.java
@@ -0,0 +1,33 @@
+package com.projeto.coinmestre.model;
+
+import com.projeto.coinmestre.domain.ExpenseStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "expenses")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String description;
+
+    private Double value;
+
+    private String category;
+
+    private LocalDate purchaseDate;
+
+    private LocalDate dueDate;
+
+    private ExpenseStatus status;
+}
